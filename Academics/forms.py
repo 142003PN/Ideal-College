fb553fd6870ec1ba0285dcbae00c1e519a4572ea
Prosper Ngona <mukukangona@gmail.com>
2025-12-10 12:00:03 +0200

    Added CRUD for year of study

--- Academics/forms.py
+++ Academics/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import SessionYear
+from Courses.models import YearOfStudy
 
 class SessionYearForm(forms.ModelForm):
     class Meta:
@@ -8,4 +9,13 @@ class SessionYearForm(forms.ModelForm):
         widgets = {
             'year_title': forms.TextInput(attrs={'class': 'form-control'}),
             'is_current_year': forms.CheckboxInput(attrs={'class': 'form-check'}),
+        }
+
+class YearOfStudyForm(forms.ModelForm):
+    class Meta:
+        model = YearOfStudy
+        fields = ['year_title']
+
+        widgets={
+            'year_title': forms.TextInput(attrs={'class': 'form-control'}),
         }
--- Academics/views.py
+++ Academics/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_list_or_404, get_object_or_404
 from .models import SessionYear
 from django.http import *
-from .forms import SessionYearForm
+from .forms import SessionYearForm, YearOfStudyForm
 from django.contrib import messages
+from Courses.models import YearOfStudy
 # Create your views here.
 def session_years(request):
     if request.user.role == 'ADMIN':
@@ -50,3 +51,29 @@ def edit_session_year(request, pk):
             return render(request, 'academics/add-session-year.html', {'form': form, 'year': year})
     else:
         return HttpResponse('<h1>Insufficient Roles</h1>')
+
+def years_of_study(request):
+    years = YearOfStudy.objects.all()
+    return render(request, 'admin/dashboard.html', {'years':years})
+
+def add_year_of_study(request):
+    if request.user.role != 'STUDENT':
+        form = YearOfStudyForm()
+        if request.method == 'POST':
+            form = YearOfStudyForm(request.POST)
+            if form.is_valid():
+                form.save(commit=False)
+                form.save()
+                messages.success(request, 'Year added')
+            else:
+                messages.error(request, 'Invalid Data')
+    else:
+        form = YearOfStudyForm()
+    return render(request, 'academics/add-year-of-study.html', {'form':form})
+
+def years_of_study(request):
+    if request.user.role != 'STUDENT':
+        years = YearOfStudy.objects.all()
+    else:
+        return HttpResponse('<h1> Insufficient Roles </h1>')
+    return render(request, 'academics/years-of-study.html', {'years':years})
