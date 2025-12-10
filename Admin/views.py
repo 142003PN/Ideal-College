e8771068eeec528888c26899c83f42a21415c80c
Prosper Ngona <mukukangona@gmail.com>
2025-12-10 12:01:57 +0200

    Added Analyticts to Admin dashboard

--- Admin/views.py
+++ Admin/views.py
@@ -1,5 +1,32 @@
 from django.shortcuts import render
+from Courses.models import *
+from Academics.models import *
+from Users.models import CustomUser
 
 # Create your views here.
 def dashboard(request):
-    return render(request, 'admin/dashboard.html')
+    if request.user.role == 'ADMIN':
+        years = YearOfStudy.objects.all()
+        session_years = SessionYear.objects.all()
+
+        #count some data
+        student_count = CustomUser.objects.filter(role='STUDENT').count()
+        staff_count = CustomUser.objects.filter(role='STAFF').count()
+        course_count = Courses.objects.all().count
+        program_count = Programs.objects.all().count()
+
+        context = {
+            'years':years,
+            'session_years':session_years,
+            'student_count':student_count,
+            'staff_count':staff_count,
+            'course_count':course_count,
+            'program_count':program_count,
+            }
+    else:
+        pass
+    return render(request, 'admin/dashboard.html', context)
+
+def years_of_study(request):
+    years = YearOfStudy.objects.all()
+    return render(request, 'admin/dashboard.html', {'years':years})
