97e2669df0f1fb88b4c87396b1fbee2d77966b23
Prosper Ngona <mukukangona@gmail.com>
2025-12-17 14:36:11 +0200

    Added home page

--- Project/urls.py
+++ Project/urls.py
@@ -3,15 +3,17 @@ from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
+from . import views
 
 urlpatterns = [
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root':settings.MEDIA_ROOT}),
     re_path(r'^static/(?P<path>.*)$', serve, {'document_root':settings.STATIC_ROOT}),
+    path('', views.index, name="index"),
     path('adminn/', admin.site.urls),
     path('departments', include('Departments.urls')),
     path('programs/', include('Programs.urls')),
     path('courses/', include('Courses.urls')),
-    path('', include('Users.urls')),
+    path('auth/', include('Users.urls')),
     path('students/', include('Students.urls')),
     path('staff/', include('Staff.urls')),
     path('applications/', include('Application_System.urls')),
