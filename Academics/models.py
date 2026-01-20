7f7ff9c507be4c9e52cbb1ce79551548ffb2f7c7
Mukuka Ngona <mukukangona@gmail.com>
2026-01-20 10:07:15 +0200

    Moved YearOfStudy to Academics App

--- Academics/models.py
+++ Academics/models.py
@@ -1,10 +1,45 @@
 from django.db import models
-
+from Users.models import CustomUser
+import uuid
 # Create your models here.
+
+#year of study
+class YearOfStudy(models.Model):
+    id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+    year_title = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.year_title
+
+#Intakes
+class Intake(models.Model):
+    class IntakeChoices(models.TextChoices):
+        January = 'January'
+        May = 'July'
+    intake_name=models.CharField(max_length=20, choices=IntakeChoices.choices, null=True, unique=True)
+    date=models.DateTimeField(auto_now_add=True)
+    added_by = models.ForeignKey(CustomUser, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.intake_name
+
 class SessionYear(models.Model):
     year_title=models.CharField(max_length=20, null=True)
+    intake = models.ForeignKey(Intake, on_delete=models.CASCADE, null=True, related_name='intakes')
     is_current_year=models.BooleanField(default=False)
     date=models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.year_title
+    
+#semesters
+class Semester(models.Model):
+    class SemesterChoices(models.TextChoices):
+        FIRST = 'first'
+        SECOND = 'second'
+    semester_name=models.CharField(max_length=20, choices=SemesterChoices.choices, null=True, unique=True)
+    date=models.DateTimeField(auto_now_add=True)
+    added_by = models.ForeignKey(CustomUser, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):  
+        return self.semester_name
--- Courses/models.py
+++ Courses/models.py
@@ -3,15 +3,10 @@ import uuid
 import json
 from Programs.models import Programs
 from Staff.models import *
+from Academics.models import YearOfStudy, Semester
 
 # Create your models here.
-class YearOfStudy(models.Model):
-    id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
-    year_title = models.CharField(max_length=50)
-    #is_current_year = models.BooleanField(default=False)
 
-    def __str__(self):
-        return self.year_title
 class Courses(models.Model):
     id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
     course_code = models.CharField(max_length=20, unique=True)
@@ -19,7 +14,11 @@ class Courses(models.Model):
     program_id = models.ForeignKey(Programs, related_name='programs', null=True, blank=True, on_delete=models.CASCADE)
     year_of_study = models.ForeignKey(YearOfStudy, blank=True, null=True, on_delete=models.CASCADE)
     date_added = models.DateTimeField(auto_now_add=True)
+    theory_hours = models.IntegerField(null=True, blank=True)
+    practical_hours = models.IntegerField(null=True, blank=True)
+    semester = models.ForeignKey(Semester, on_delete=models.SET_NULL, null=True, blank=True)
     added_by=models.ForeignKey(Staff, on_delete=models.SET_NULL, null=True, blank=True)
+
 
     def __str__(self):
         return self.course_code +'--'+ self.course_title
